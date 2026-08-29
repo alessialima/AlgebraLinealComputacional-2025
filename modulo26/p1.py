683fe35948cdeafeb195ffb639c82cd133f24b7a
alessia <lima.alessialourdes@gmail.com>
2026-08-29 11:16:26 -0300

    Update p1.py

--- modulo26/p1.py
+++ modulo26/p1.py
@@ -5,20 +5,26 @@ Practica 1:
 import numpy as np 
 
 def error(x,y): 
+  x = np.float64(x)
+  y = np.float64(x)
   return abs(x-y)
 
 def error_relativo(x,y): 
+  x = np.float64(x)
+  y = np.float64(x)
   if x == 0: 
-    return error(x,y) # no me acuerdo si era asi o dejabamos q se rompa y ya fue 
+    return error(x,y) 
   return abs(x-y)/abs(x) 
 
 def matricesIguales(A, B): 
-  # paso 1: miro las dimensiones 
+  A = np.array(A, dtype=np.float64)
+  B = np.array(B, dtype=np.float64)
+
   a, b = A.shape 
   m, n = B.shape 
   if a != m or b != n: 
      return False 
-  # paso 2: miro los elementos 
+    
   for i in range(a): 
     for j in range(b): 
       if error(A[i][j], B[i][j]) > 1e-08: 
@@ -28,3 +34,11 @@ def matricesIguales(A, B):
 """ 
 Practica 2
 """
+
+
+
+
+
+
+
+
